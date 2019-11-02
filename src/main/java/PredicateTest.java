7148daaf505a357f743dd7e5800c4c7e94531c64
yanzeyu <yanzeyu@aspirecn.com>
2019-11-02 23:47:23 +0800

    HashSet容器测试

--- src/main/java/PredicateTest.java
+++ src/main/java/PredicateTest.java
@@ -1,3 +1,7 @@
+import com.alibaba.fastjson.JSON;
+
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.function.Predicate;
 
 public class PredicateTest {
@@ -5,5 +9,16 @@ public class PredicateTest {
 		Predicate<Integer> predicate = i -> i>10;
 
 		System.out.println(predicate.test(5));
+
+		HashSet<String> hashSet = new HashSet<>();
+
+		hashSet.add("hello");
+
+		hashSet.add("world");
+		hashSet.add("1111");
+		hashSet.add("hello");
+
+		Object json = JSON.toJSON(hashSet);
+		System.out.println(json);
 	}
 }
