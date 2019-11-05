e061d400456ad712ca9d6aed01c4e2d49049cb61
yanzeyu <yanzeyu@aspirecn.com>
2019-11-05 16:37:03 +0800

    Optional函数测试

---
+++ src/main/java/optionalTest.java
@@ -0,0 +1,13 @@
+import java.util.Optional;
+
+public class optionalTest {
+	public static void main(String[] args) {
+		Optional<String> optional = Optional.ofNullable("hello");
+
+		optional.ifPresent((o) ->System.out.println(o));
+
+		System.out.println(optional.orElse("world"));
+
+		System.out.println(optional.orElseGet(() -> "111111"));
+	}
+}
