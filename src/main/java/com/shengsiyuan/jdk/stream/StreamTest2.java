a2f21d8d1835e1863895c93ba5d7cd48921c7dfd
yanzeyu <13144454225>
2019-11-17 23:48:50 +0800

    stream测试

---
+++ src/main/java/com/shengsiyuan/jdk/stream/StreamTest2.java
@@ -0,0 +1,24 @@
+package com.shengsiyuan.jdk.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamTest2 {
+    public static void main(String[] args) {
+        Stream<String> stream = Stream.of("hello","world","hello world");
+        String[] strings = stream.toArray(length -> new String[length]);
+//        String[] strings = stream.toArray(String[]::new);
+//        Arrays.asList(strings).forEach(System.out::println);
+
+//        List<String> list = stream.collect(Collectors.toList());
+        //List<String> list = stream.collect(LinkedList::new,LinkedList::add,LinkedList::addAll);
+        List<String> list = stream.collect(() -> new ArrayList(),
+                (theList,item) -> theList.add(item),
+                (resultList,theList1) -> resultList.addAll(theList1));
+        list.forEach(System.out::println);
+    }
+}
